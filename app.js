9cdea7e32239643083b5511b592d26841fd3d3dc
Rajdip9999 <101238383+Rajdip9999@users.noreply.github.com>
2026-05-11 13:02:11 +0530

    add weplayId

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {MongoClient} from 'mongodb'
+import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import path from 'path';
@@ -18,29 +18,29 @@ const clients = new MongoClient(url)
 
 let db;
 
-const ConnectDB = async()=>{
+const ConnectDB = async () => {
 
-    try{
-    await clients.connect()
-    db = clients.db('raaz')
-    console.log('connect to db')
+    try {
+        await clients.connect()
+        db = clients.db('raaz')
+        console.log('connect to db')
 
-    const lists = await db.collection('list')
+        const lists = await db.collection('list')
 
-     await lists.createIndex(
+        await lists.createIndex(
             { name: 1 },
             { unique: true, partialFilterExpression: { name: { $exists: true } } }
         );
 
-    app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`)
 
-    })
+        })
 
-}
+    }
 
 
-    catch(err){
+    catch (err) {
         console.log(err.message)
     }
 }
@@ -53,51 +53,52 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // POST Route (Moved outside of the GET route)
-app.post('/req', async(req, res) => {
-    const {name} = req.body;
-    if(!name || name.trim()===''){
+app.post('/req', async (req, res) => {
+    const { name } = req.body;
+    if (!name || name.trim() === '') {
 
-       return res.status(400).send('Please Enter Valid name')
+        return res.status(400).send('Please Enter Valid name')
     }
 
-    try{
+    try {
 
-    const collection = await db.collection('list')
+        const collection = await db.collection('list')
 
-    await collection.insertOne({name:name})
+        await collection.insertOne({ name: name })
 
-    res.status(200).send('thanks for submit');
+        res.status(200).send('thanks for submit');
 
     }
 
-    catch(err){
-        if(err.code ===11000){
+    catch (err) {
+        if (err.code === 11000) {
             res.status(400).send('এই নাম আগের থেকেই আছে দয়া করে নতুন নাম দিন, ধন্যবাদ')
         }
     }
 });
 
 // GET Route
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     const { name, adress } = req.query; // Note: 'adress' matches your query param
 
 
     const list = await db.collection('list')
 
-    const friends = await list.find({name:`${name}`}).toArray()
+    const friends = await list.find({ name: `${name}` }).toArray()
 
 
-if (friends.length > 0) {
+    if (friends.length > 0) {
 
-    const friend = friends[0]
+        const friend = friends[0]
 
-    return res.json({
-        age: friend.age,
-        iselegible:friend.iselegible
-    })
+        return res.json({
+            age: friend.age,
+            iselegible: friend.iselegible,
+            weplayId: friend.weplayid
+        })
 
 
-} else if (adress) {
+    } else if (adress) {
         res.send(`This is adress ${adress}`);
     } else {
         res.status(200).send('Bye');
