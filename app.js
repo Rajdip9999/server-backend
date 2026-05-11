b46cad470e2120fdfb20aeab26df9212f1ebc53c
Rajdip9999 <101238383+Rajdip9999@users.noreply.github.com>
2026-05-11 11:44:08 +0530

    backend server

---
+++ app.js
@@ -0,0 +1,104 @@
+import express from 'express';
+import {MongoClient} from 'mongodb'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import path from 'path';
+const app = express();
+
+dotenv.config()
+
+app.use(cors())
+//Mongo Db 
+
+const url = process.env.MONGO_URL
+
+const port = process.env.PORT
+const clients = new MongoClient(url)
+
+let db;
+
+const ConnectDB = async()=>{
+
+    try{
+    await clients.connect()
+    db = clients.db('raaz')
+    console.log('connect to db')
+
+    const lists = await db.collection('list')
+
+     await lists.createIndex(
+            { name: 1 },
+            { unique: true, partialFilterExpression: { name: { $exists: true } } }
+        );
+
+    app.listen(port, () => {
+    console.log('Server is running on port 80');
+
+    })
+
+}
+
+
+    catch(err){
+        console.log(err.message)
+    }
+}
+
+ConnectDB()
+
+
+// Middleware setup
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// POST Route (Moved outside of the GET route)
+app.post('/req', async(req, res) => {
+    const {name} = req.body;
+    if(!name || name.trim()===''){
+
+       return res.status(400).send('Please Enter Valid name')
+    }
+
+    try{
+
+    const collection = await db.collection('list')
+
+    await collection.insertOne({name:name})
+
+    res.status(200).send('thanks for submit');
+
+    }
+
+    catch(err){
+        if(err.code ===11000){
+            res.status(400).send('এই নাম আগের থেকেই আছে দয়া করে নতুন নাম দিন, ধন্যবাদ')
+        }
+    }
+});
+
+// GET Route
+app.get('/', async(req, res) => {
+    const { name, adress } = req.query; // Note: 'adress' matches your query param
+
+
+    const list = await db.collection('list')
+
+    const friends = await list.find({name:`${name}`}).toArray()
+
+
+if (friends.length > 0) {
+
+    const friend = friends[0]
+
+    return res.json({
+        age: friend.age,
+        iselegible:friend.iselegible
+    })
+
+
+} else if (adress) {
+        res.send(`This is adress ${adress}`);
+    } else {
+        res.status(200).send('Bye');
+    }
+});
